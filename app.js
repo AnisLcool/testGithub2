7700ee1f3e9be0f56ba24e7fff7940cb47768c66
Anis <mohamedrbh519@gmail.com>
2022-07-05 13:20:56 +0100

    Titre : ajout fonctionnalité bouton description : pour afficher un alert

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-console.log('hello world!');
-console.log('bonjour');
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
-}
-
-const nums = [1,2,3]
+document.querySelector('button').addEventListener('click', function(){
+    alert('Bouton!!!')
+})
